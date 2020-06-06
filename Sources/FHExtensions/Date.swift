f8eb40a9352482d49a1eb29e48f1976a652ed68a
Felix Herrmann <felix.herrmann@icloud.com>
2020-06-06 05:13:17 +0200

    Date init? with date components

---
+++ Sources/FHExtensions/Date.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by FHRApps on 06.06.20.
+//
+
+import Foundation
+
+extension Date {
+    init?(_ day: Int, _ month: Int, _ year: Int, hour: Int? = nil, minute: Int? = nil, second: Int? = nil) {
+        let dateComponents = DateComponents(year: year, month: month, day: day, hour: hour, minute: minute, second: second)
+        guard let date = Calendar.current.date(from: dateComponents) else {
+            return nil
+        }
+        
+        self = date
+    }
+}
--- Tests/FHExtensionsTests/FHExtensionsTests.swift
+++ Tests/FHExtensionsTests/FHExtensionsTests.swift
@@ -13,6 +13,18 @@ final class FHExtensionsTests: XCTestCase {
         XCTAssertNil(array[safe: 2], "value is nill")
     }
     
+    func testDateInit() {
+        guard let date = Date(23, 2, 1999, hour: 9, minute: 41, second: 0) else { return }
+        let components = Calendar.current.dateComponents([.second, .minute, .hour, .day, .month, .year,], from: date)
+        
+        XCTAssertEqual(components.second, 0)
+        XCTAssertEqual(components.minute, 41)
+        XCTAssertEqual(components.hour, 9)
+        XCTAssertEqual(components.day, 23)
+        XCTAssertEqual(components.month, 2)
+        XCTAssertEqual(components.year, 1999)
+    }
+    
     func testRGBColors() {
         #if os(iOS) || os(tvOS) || os(watchOS) || targetEnvironment(macCatalyst)
         let color = UIColor(red: 10, green: 20, blue: 30, alpha: 0.5)
@@ -31,6 +43,8 @@ final class FHExtensionsTests: XCTestCase {
     }
     
     static var allTests = [
+        ("arraySafe", testArraySafe),
+        ("dateInit", testDateInit),
         ("rgbColors", testRGBColors),
         ("modelIdentifier", testModelIdentifier)
     ]
