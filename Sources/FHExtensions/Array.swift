975bbce359aff4b893557853e148db9fc2c2cdc7
Felix Herrmann <felix.herrmann@icloud.com>
2020-06-13 21:43:08 +0200

    subscript(safe:) setter

--- Sources/FHExtensions/Array.swift
+++ Sources/FHExtensions/Array.swift
@@ -4,10 +4,19 @@ public extension Array {
     /// Accesses the element at the specified position safely.
     /// - Parameter index: The position of the element to access safely.
     @inlinable subscript(safe index: Index) -> Element? {
-        guard index >= startIndex && index < endIndex else {
-            return nil
+        get {
+            guard index >= startIndex && index < endIndex else {
+                return nil
+            }
+            
+            return self[index]
         }
-        
-        return self[index]
+        set {
+            guard let newValue = newValue, index >= startIndex && index < endIndex else {
+                return
+            }
+            
+            self[index] = newValue
+        }
     }
 }
