fa6be3fa6bd345f91ac9db6d3a0b6c3e40a29dd6
Felix Herrmann <felix.herrmann@icloud.com>
2022-02-18 23:38:31 +0100

    used macOS instead of OSX

--- Sources/FHExtensions/Foundation/JSONEncoder.swift
+++ Sources/FHExtensions/Foundation/JSONEncoder.swift
@@ -3,7 +3,7 @@ import Foundation
 extension JSONEncoder.DateEncodingStrategy {
     
     /// The strategy that formats dates according to the ISO 8601 and RFC 3339 standards with fractional seconds.
-    @available(OSX 10.13, iOS 11.0, tvOS 11.0, *)
+    @available(macOS 10.13, iOS 11.0, tvOS 11.0, *)
     public static let iso8601withFractionalSeconds = custom { date, encoder in
         let formatter = ISO8601DateFormatter()
         formatter.formatOptions = [.withFractionalSeconds, .withInternetDateTime]
